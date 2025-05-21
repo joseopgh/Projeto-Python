b8293e7eb323b02faa76fa41772ac12f83d46bd9
JoseDSJunior <145411583+joseopgh@users.noreply.github.com>
2025-05-21 19:44:32 -0300

    Ex006DobroTriploRaiz
    Atualização do Comit

---
+++ ExercBasic/Ex006DobroTriploRaiz.py
@@ -0,0 +1,4 @@
+num = int(input('Digte um Número: '))
+print ('O dobro de {} vale {}' .format (num, (num*2)))
+print ('O triplo de {} Vale {}' .format (num, (num*3)))
+print ('A raiz quadrade de {} vale {:.2f}' .format (num, (num**(1/2))))
