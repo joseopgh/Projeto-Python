b780f27aefc8f3ea9ad5bdab3de136c9b8b3061c
JoseDSJunior <145411583+joseopgh@users.noreply.github.com>
2025-05-21 19:21:31 -0300

    Ex005
    CodigoLimpo

--- ExercBasic/Ex005AntecessorSucessor.py
+++ ExercBasic/Ex005AntecessorSucessor.py
@@ -1,4 +1,2 @@
 n1 = int(input('Digite um Numero! '))
-a = n1 - 1
-b = n1 + 1
-print('Analizando o valor {}, seu antecessor é {} e o sucessor é {}' .format (n1, a, b))
+print('Analizando o valor {}, seu antecessor é {} e o sucessor é {}' .format (n1, (n1-1), (n1+1)))
