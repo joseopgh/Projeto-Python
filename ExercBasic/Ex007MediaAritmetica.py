ac92fe28f67c286aa6b851d1d46ca727a29d1ea7
JoseDSJunior <145411583+joseopgh@users.noreply.github.com>
2025-05-21 20:04:19 -0300

    ex007

--- ExercBasic/Ex007MediaAritmetica.py
+++ ExercBasic/Ex007MediaAritmetica.py
@@ -1,4 +1,4 @@
 nota1 = float(input('Primeira nota do aluno: '))
 nota2 = float(input('Segunda nota do aluno: '))
 media = (nota1+nota2)/2
-print('A media entre {} e {} é igual a {:.2f}' .format(nota1, nota2, media))
+print('A media entre {} e {} é igual a {:.1f}' .format(nota1, nota2, media))
