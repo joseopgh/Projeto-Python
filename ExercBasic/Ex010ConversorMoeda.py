2cba989a854f215ad586ee5238a08807160b94f3
JoseDSJunior <145411583+joseopgh@users.noreply.github.com>
2025-05-23 22:18:28 -0300

    Ex010Converção
    Add Euro na Converção

--- ExercBasic/Ex010ConversorMoeda.py
+++ ExercBasic/Ex010ConversorMoeda.py
@@ -1,3 +1,5 @@
 moeda = float(input('Quanto dinheiro você tem na carteira? R$'))
-cv = moeda/3.27
-print('Com R${:.2f} você pode comprar US${:.2f} '.format(moeda, cv))
+dolar = moeda/5.65
+euro = moeda/6.42
+print('Com R${:.2f} você pode comprar US${:.2f} '.format(moeda, dolar))
+print('Com R${:.2f} Você pode comprar E${:.2f} '.format(moeda, euro))
