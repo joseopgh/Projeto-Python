935d3a7a3df8cf98ae83b93b1d9a77bca811d7f4
JoseDSJunior <145411583+joseopgh@users.noreply.github.com>
2025-05-28 20:23:46 -0300

    Ex012Calculo deDesconto
    Desconto de dinheiro

---
+++ ExercBasic/Ex012CalculandoDesconto.py
@@ -0,0 +1,3 @@
+real = float(input('Qual é o valor do produto: R$'))
+des = real - (real * 5 / 100)
+print('O produto que custava R${}, na promoção com desconto de 5% vai custar R${}' .format(real, des))
