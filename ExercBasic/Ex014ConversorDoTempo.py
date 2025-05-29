81520667f87e60d502dfe07d4078b64fb9cd23b0
JoseDSJunior <145411583+joseopgh@users.noreply.github.com>
2025-05-29 20:48:54 -0300

    Ex014
    Corrigindo Erro

--- ExercBasic/Ex014ConversorDoTempo.py
+++ ExercBasic/Ex014ConversorDoTempo.py
@@ -1,3 +1,3 @@
 Crau = float(input('informe a temperatura em Cº: '))
 conv = Crau * 1.8 + 32
-print('A temperatura de {}ºC coresponde a {}ºF!' .format(Crau, conv))
+print('A temperatura de {}ºC corresponde a {}ºF!' .format(Crau, conv))
