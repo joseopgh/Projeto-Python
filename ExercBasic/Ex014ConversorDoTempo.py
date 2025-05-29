054b85be0f6df339941be0d29dda5d60ec53250d
JoseDSJunior <145411583+joseopgh@users.noreply.github.com>
2025-05-29 20:52:19 -0300

    Ex014
    Outros modos de Fazer

--- ExercBasic/Ex014ConversorDoTempo.py
+++ ExercBasic/Ex014ConversorDoTempo.py
@@ -1,3 +1,4 @@
-Crau = float(input('informe a temperatura em Cº: '))
+Crau = float(input('Informe a temperatura em Cº: '))
 conv = Crau * 1.8 + 32
+#ou convertido = ((9*/Crau)/5+32)
 print('A temperatura de {}ºC corresponde a {}ºF!' .format(Crau, conv))
