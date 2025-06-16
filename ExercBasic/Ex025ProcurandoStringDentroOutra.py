694267624424a85ee595a1caa082b2cc3fdc9c29
JoseDSJunior <145411583+joseopgh@users.noreply.github.com>
2025-06-16 19:32:07 -0300

    Ex025ProcuradnoString
    Acresentando novas forma de fazer

--- ExercBasic/Ex025ProcurandoStringDentroOutra.py
+++ ExercBasic/Ex025ProcurandoStringDentroOutra.py
@@ -1,3 +1,4 @@
 nome = str(input('Qual é seu nome completo? ')).strip()
 name = nome.lower()
 print('Seu nome tem Silva? {} '.format(name.find('silva')>0))
+#print('Seu nome tem Silva? {}' .format('silva' in nome.lower()))
