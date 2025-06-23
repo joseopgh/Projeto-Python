8fd3bde1daee06907673172a539e3a4f9829e5ed
JoseDSJunior <145411583+joseopgh@users.noreply.github.com>
2025-06-23 19:28:04 -0300

    Ex029RadarEletronico
    Melhoria do Programa com menos linhas

--- ExercBasic/Ex029RadarEletronico.py
+++ ExercBasic/Ex029RadarEletronico.py
@@ -2,6 +2,4 @@ km = int(input('Qual é a velocidade atual do carro? '))
 if km > 80:
     print('MULTADO! Você excedeu o limite de velocidade de 80km/h')
     print('Você deve pagar uma multa de R${:.2f}'.format((km - 80)*7))
-    print('Tenha um bom dia! Dirija com segurança!')
-else:
-    print('Tenha um bom dia! Dirija com segurança!')
+print('Tenha um bom dia! Dirija com segurança!')
