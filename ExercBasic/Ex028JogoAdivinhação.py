cf5d52b05bbf2ede36fc03ed24a3d8526f217d75
JoseDSJunior <145411583+joseopgh@users.noreply.github.com>
2025-06-23 19:04:15 -0300

    Ex028JogoAdivinhação
    Acresentado o Sleep para tempo de espera

--- ExercBasic/Ex028JogoAdivinhação.py
+++ ExercBasic/Ex028JogoAdivinhação.py
@@ -1,10 +1,14 @@
 from random import randint
+from time import sleep
 computador = randint(0,5)            # Faz o computador "Pensar"
 print('-=-' *20)
 print('Vou pensar em um numero entre 0 e 5. Tente adivinhar...')
 print('-=-' *20)
 jogador = int(input('Em que número pensei? ')) #Jogador tenta adivinhar
+print('PROCESSANDO...')
+sleep(3)
 if jogador == computador:
     print('PARABENS! você conseguiu me vencer!')
 else:
     print('GANHEI! Eu pensei no número {} e não no {}.' .format(computador, jogador))
+
