cc6535ace4c7bfef66db3887d382be35e81a67c8
JoseDSJunior <145411583+joseopgh@users.noreply.github.com>
2025-06-24 20:13:08 -0300

    Ex032AnoBissexto
    Acrescentando comentarios

--- ExercBasic/Ex032AnoBissexto.py
+++ ExercBasic/Ex032AnoBissexto.py
@@ -1,7 +1,7 @@
-from datetime import date
+from datetime import date                               #Importa o ano atual para o codigo
 ano = int(input('Qua ano quer analizar? Coloque 0 para analizar o ano atual: '))
 if ano == 0:
-    ano = date.today().year
+    ano = date.today().year                            #Para saber o ano atual da maquina
 if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:    #Calculo para saber se o ano é bisxesto ou não
     print('O ano {} é BISSEXTO' .format(ano))
 else:
