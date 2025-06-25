d1eb39ef99be1d605c032ed34ba3a43c42b57192
JoseDSJunior <145411583+joseopgh@users.noreply.github.com>
2025-06-25 19:20:43 -0300

    Ex033MenorMaior
    Melhoria na leitura do programa

--- ExercBasic/Ex033MaiorMenosValor.py
+++ ExercBasic/Ex033MaiorMenosValor.py
@@ -3,17 +3,15 @@ num2 = int(input('Segundo Valor: '))
 num3 = int(input('Terceiro Valor: '))
 #Verificando quem é menor
 menor = num1
-if num2<num1 and num2<num3:
+if num2 < num1 and num2 < num3:
     menor = num2
-if num3<num1 and num3<num2:
+if num3 < num1 and num3 < num2:
     menor = num3
 #Verificando quem é maior
 maior = num1
-if num2>num1 and num2>num3:
+if num2 > num1 and num2 > num3:
     maior = num2
-if num3>num1 and num3>num2:
+if num3 > num1 and num3 > num2:
     maior = num3
 print('O menor valor digitado foi {}'.format(menor))
 print('O maior valor digitado foi {}'.format(maior))
-
-
