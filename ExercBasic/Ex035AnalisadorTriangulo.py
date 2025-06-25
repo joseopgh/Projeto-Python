b44483722f36eaea56b322586c027853c56d5b6f
JoseDSJunior <145411583+joseopgh@users.noreply.github.com>
2025-06-25 20:00:50 -0300

    Ex035FormaTriangulo
    Melhoria do progama

--- ExercBasic/Ex035AnalisadorTriangulo.py
+++ ExercBasic/Ex035AnalisadorTriangulo.py
@@ -4,3 +4,7 @@ print('=-'*20)
 a = float(input('Primeiro segmento: '))
 b = float(input('segundo segmento: '))
 c = float(input('Terceiro segmento: '))
+if a < b + c and b < c + a and c < a + b:
+    print('Os segmentos acima PODEM FORMA triangulo!')
+else:
+    print('Os segmentos a cima Não PODEM FORMA triangulo!')
