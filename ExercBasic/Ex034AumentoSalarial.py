493edcf4c2c4ab63c72a086669955ae9e2937296
JoseDSJunior <145411583+joseopgh@users.noreply.github.com>
2025-06-27 20:48:20 -0300

    Ex034CalculoSalarial
    Adicionando cores

--- ExercBasic/Ex034AumentoSalarial.py
+++ ExercBasic/Ex034AumentoSalarial.py
@@ -1,5 +1,5 @@
 salario = int(input('Qual éo salario do funcionario? R$'))
 if salario <= 1250:
-    print('Quem ganhava R${} passa a ganhar R${:.2f} agora'.format(salario, salario + (salario * 0.15)))
+    print('Quem ganhava R$\033[1;31m{}\033[m passa a ganhar R$\033[1;34m{:.2f}\033[m agora'.format(salario, salario + (salario * 0.15)))
 else:
-    print('Quem ganhava R${} passa a ganhar R${:.2f} agora'.format(salario, salario + (salario * 0.10)))
+    print('Quem ganhava R$\033[1;31m{}\033[m passa a ganhar R$\033[1;34m{:.2f}\033[m agora'.format(salario, salario + (salario * 0.10)))
