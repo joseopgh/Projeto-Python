bfb0891827dae60bbc64400d5c48f83d287a9af9
JoseDSJunior <145411583+joseopgh@users.noreply.github.com>
2025-07-09 20:35:38 -0300

    Ex037ConvercaoBaseNumerica

---
+++ ExercBasic/Ex037ConversaoBaseNumerica.py
@@ -0,0 +1,19 @@
+num = int(input('Digite um número inteiro: '))
+print('Escolha uma das base para conversão:')
+print('[ 1 ] converter para Binario')
+print('[ 2 ] converter para Octal')
+print('[ 3 ] converter para Hexadecimal')
+opcao = int(input('Sua opção: '))
+
+binario = bin(num)
+octal = oct(num)
+hexadecimal = hex(num)
+
+if opcao == 1:
+    print('{} convertido para Binario é igual a {}' .format(num, binario))
+elif opcao == 2:
+    print('{} convertido para Octal é igual a {}' .format(num, octal))
+elif opcao == 3:
+    print('{} convertido para Hexadecimal é igual a {}' .format(num, hexadecimal))
+else:
+    print('A Opção digitada esta incorreta.')
