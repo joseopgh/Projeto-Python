e0bd84edbc1293759c140838bf8d4c65508a4b9f
JoseDSJunior <145411583+joseopgh@users.noreply.github.com>
2025-07-10 18:58:40 -0300

    Ex038Comparaçãonumerica
    Edição do print

--- ExercBasic/Ex038CamparandoNumero.py
+++ ExercBasic/Ex038CamparandoNumero.py
@@ -1,8 +1,8 @@
 num1 = int(input('Primeiro numero: '))
 num2 = int(input('Segundo numero: '))
 if num1 > num2:
-    print('O primeiro numero é maior')
+    print('O PRIMEIRO numero é maior')
 elif num2 > num1:
-    print('O segundo numero é maior')
+    print('O SEGUNDO numero é maior')
 else:
     print('Os dois valores são iguais')
