0296835fc457b0c79a72435ebbb4cc11406410cb
JoseDSJunior <145411583+joseopgh@users.noreply.github.com>
2025-07-10 18:54:31 -0300

    Ex038ComparacaoNumerica
    Descida qual dos numero são maiores

---
+++ ExercBasic/Ex038CamparandoNumero.py
@@ -0,0 +1,8 @@
+num1 = int(input('Primeiro numero: '))
+num2 = int(input('Segundo numero: '))
+if num1 > num2:
+    print('O primeiro numero é maior')
+elif num2 > num1:
+    print('O segundo numero é maior')
+else:
+    print('Os dois valores são iguais')
