8c3a91eae3f55d4e3ce1a42ae0294e448c9ca058
JoseDSJunior <145411583+joseopgh@users.noreply.github.com>
2025-07-10 19:52:07 -0300

    Ex039AlistamentoMilitar
    CorringindoErro

--- ExercBasic/Ex039AlistamentoMilitar.py
+++ ExercBasic/Ex039AlistamentoMilitar.py
@@ -6,8 +6,8 @@ print('Quem nasceu em {} tem {} ano em {}.'.format(nasceu, idade, ano))
 if idade == 18:
     print('Você tem que se alistar \033[0;31mIMEDIATAMENTE!\033[m')
 elif idade > 18:
-    print('Você ja deveria ter se alistado há {} anos.'.format(idade -18))
+    print('Você ja deveria ter se alistado há {} anos.'.format(idade - 18))
     print('Seu alistamento foi em {}.'.format(nasceu + 18))
 else:
     print('Ainda falta {} anos para o alistamento' .format(18 - idade))
-    print('Seu alistamento foi em {}.'.format(nasceu + 18))
+    print('Seu alistamento será em {}.'.format(nasceu + 18))
