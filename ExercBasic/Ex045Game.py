3e8493427547baa1ac9dcff307a22c2b92ca1a70
JoseDSJunior <145411583+joseopgh@users.noreply.github.com>
2025-07-17 20:10:42 -0300

    Ex045Gamer
    Melhoria do gamer

--- ExercBasic/Ex045Game.py
+++ ExercBasic/Ex045Game.py
@@ -1,23 +1,50 @@
 import time
-import random
+from random import randint
+itens = ('Pedra', 'Papel', 'Tesoura')
+computador = randint (0, 2)
+print('O computador escolhei {}'.format(itens[computador]))     ##Estrutudo do codigo
 print ('''Suas opção:
 [ 0 ] PEDRA
 [ 1 ] PAPEL
 [ 2 ] TESOURA''')
+
 jogada = int(input('Qual a sua jogada? '))
 print ('jo')
 time.sleep(1)
-print ('KEM')
+print ('KEM')                                                       ##Time sleep de contador do gamer
 time.sleep(1)
 print ('POW')
 time.sleep(1)
-print ('-='*15)
-lista = [0, 1 , 2]
 
-escolhido = random.choice(lista)
-if jogada == 1 and escolhido == 0 or jogada == 2 and escolhido == 1:
-    print ('COMPUTADOR jogou {}'.format(escolhido))
-    print ('JOGADOR jogou {}'.format(jogada))
-elif jogada == 0 and escolhido == 1 or jogada == 1 and escolhido == 2:
-    print ('COMPUTADOR jogou {}'.format(escolhido))
-    print ('JOGADOR jogou {}'.format(jogada))
+print ('-='*15)
+print('O computador escolhei {}'.format(itens[computador]))           ##Emprimir na tela o resultado do jogo
+print('O jogador jogou {}'.format(itens[jogada]))
+print ('-='*15)
+
+if computador == 0:
+    if jogada == 0:
+        print('EMPATE')
+    elif jogada == 1:
+        print('JOGADOR VENCE')
+    elif jogada == 2:
+        print('COMPUTADOR VENCE')
+    else:
+        print('JOGADA INVALIDA')
+elif computador == 1:
+    if jogada == 0:
+        print('COMPUTADOR VENCE')
+    elif jogada == 1:
+        print('EMPATE')
+    elif jogada == 2:
+        print('JOGADOR VENCE')
+    else:
+        print('JOGADA INVALIDA')
+elif computador == 2:
+    if jogada == 0:
+        print('JOGADOR VENCE')
+    elif jogada == 1:
+        print('COMPUTADOR VENCE')
+    elif jogada == 2:
+        print('EMPATE')
+    else:
+        print('JOGADA INVALIDA')
