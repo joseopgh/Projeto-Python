69b26cf66f89325ad8f50c851e96d16d7b6be80a
JoseDSJunior <145411583+joseopgh@users.noreply.github.com>
2025-07-23 19:52:36 -0300

    Ex050SomaDosPares
    Melhoria do valores

--- ExercBasic/Ex050SomaDosPares.py
+++ ExercBasic/Ex050SomaDosPares.py
@@ -1,4 +1,4 @@
-for c in range(0, 6):
-    num = int(input('Digite um valor: '))
+for c in range(1, 7):
+    num = int(input('Digite o {}º valor: '.format(c)))
 if num % 2 == 0:
     print('A soma dos numeros pares é \033[0;31m{}\033[m' .format(num + num))
