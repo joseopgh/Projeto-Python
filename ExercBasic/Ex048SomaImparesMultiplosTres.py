4c5b9a9437e91c44729906f55e67eed2ff4be8b1
JoseDSJunior <145411583+joseopgh@users.noreply.github.com>
2025-07-23 19:35:27 -0300

    Ex049Tabuada2.0

--- ExercBasic/Ex048SomaImparesMultiplosTres.py
+++ ExercBasic/Ex048SomaImparesMultiplosTres.py
@@ -1,5 +1,7 @@
 soma = 0
+cont = 0
 for num in range(1, 501, 2):
     if num % 3 == 0:
+        cont = cont + 1
         soma = soma + num
-print('A soma de todos os valores solicitado é {}'.format(soma))
+print('A soma de todos os {} valores solicitado é {}'.format(cont, soma))
