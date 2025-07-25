7c03b02b39adcae589609a266c2d366c5d85a12b
JoseDSJunior <145411583+joseopgh@users.noreply.github.com>
2025-07-25 19:58:07 -0300

    Ex051ProgressaoAritimetrica
    Melhoreias dos  lups

--- ExercBasic/Ex051ProgressaoEritmetica.py
+++ ExercBasic/Ex051ProgressaoEritmetica.py
@@ -3,6 +3,7 @@ print('     10 TERMOS DE UMA PA')
 print('='*30)
 termo = int(input('Primeiro termo: '))
 razao = int(input('Razão: '))
-for c in range(termo, 11, razao):
-    print('{} ->'.format(c), end=' ')
+decimo = termo + (10 - 1) * razao
+for c in range(termo, decimo + razao, razao):
+    print('{}'.format(c), end=' -> ')
 print('ACABOU')
