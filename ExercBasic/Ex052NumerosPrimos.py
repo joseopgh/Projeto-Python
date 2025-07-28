c71aeddd13ffbd32d766b7a28af06bcbca720765
JoseDSJunior <145411583+joseopgh@users.noreply.github.com>
2025-07-28 19:28:58 -0300

    Ex052NumeroPrimos
    Ache o numero primo

---
+++ ExercBasic/Ex052NumerosPrimos.py
@@ -0,0 +1,9 @@
+num = int(input('Digite um número: '))
+if num > 1:
+    for c in range(2, int(num **  0.5) + 1):
+        if num %  == 0:
+            print(c, end=' ')
+        else:
+            print('Não é primo')
+else:
+    print('O numero digitado não é primo pois é menos que 1')
