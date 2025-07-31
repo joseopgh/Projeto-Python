4becf968bdad8abb4220cd6be9ea6f0d6f6e5433
JoseDSJunior <145411583+joseopgh@users.noreply.github.com>
2025-07-31 19:48:24 -0300

    Ex055MaiorMenosPeso
    Corrigindo Erro dos mesmo

--- ExercBasic/Ex055MaiorMEnos.py
+++ ExercBasic/Ex055MaiorMEnos.py
@@ -1,5 +1,14 @@
 maiorpeso = 0
 menorpeso = 0
 for pessoa in range(1,6):
-    peso = float(input('Peso da {}ª pessoa: '.format(pessoa+1)))
-    if peso 
+    peso = float(input('Peso da {}ª pessoa: '.format(pessoa)))
+    if pessoa == 1:
+        maiorpeso = peso
+        menorpeso = peso
+    else:
+        if peso > maiorpeso:
+            maiorpeso = peso
+        if peso < menorpeso:
+            menorpeso = peso
+print('O maior peso lido foi de {}Kg'.format(maiorpeso))
+print('O menos peso lido foi de {}kg'.format(menorpeso))
