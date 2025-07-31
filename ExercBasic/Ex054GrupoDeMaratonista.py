2f7e2382afc7c2da7c79bc52f6d3d5f025784ea1
JoseDSJunior <145411583+joseopgh@users.noreply.github.com>
2025-07-31 19:24:42 -0300

    Ex054GrupoDeMaratonista
    Corrigindo Erro do mesmo

--- ExercBasic/Ex054GrupoDeMaratonista.py
+++ ExercBasic/Ex054GrupoDeMaratonista.py
@@ -1,7 +1,13 @@
 from datetime import date
 idade = date.today().year
+totmaior = 0
+totmenos = 0
 for c in range(0,7):
     data = int(input('Em que ano a {}ª pessoa nasceu? '.format(c+1)))
     ano = idade - data
-print('''Ao todo tivemos {} pessoas maiores de idade
-E Também tivemos {} pessoa menores de idade'''.format)
+    if ano >= 21:
+        totmaior += 1
+    else:
+        totmenos += 1
+print('''Ao todo tivemos {} pessoal maior de idade
+E também tivemos {} pessoa menos de idade'''.format(totmaior, totmenos))
