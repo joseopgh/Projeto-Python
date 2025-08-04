0e84866b33feb02a5d1549b2069e449120c2bb96
JoseDSJunior <145411583+joseopgh@users.noreply.github.com>
2025-08-04 20:09:40 -0300

    Ex056AnalisandorCompleto
    Corrigindo os erro e acrescentado melhorias no mesmo

--- ExercBasic/Ex056AnalisadorCompleto.py
+++ ExercBasic/Ex056AnalisadorCompleto.py
@@ -1,11 +1,27 @@
 media = 0
+mediaidade = 0
+maioridadehomem = 0
+nomevelho = ''
+totmulher20 = 0
+
 for pessoa in range (1,5):
     print('----- {}ª \033[1;36mPESSOA\033[m -----'.format(pessoa))
-    name = str(input('Nome: '))
+    nome = str(input('Nome: ')).strip()  #strip para tirar os espaços
     idade = int(input('Idade: '))
-    Sexo = str(input('sexo [M/F]: '))
+    sexo = str(input('sexo [M/F]: ')).strip()
 
-    media += pessoa
-    idmedia = media / 4
+    media = media + idade  #pode usar tbm o "+=" Ex media += idade
+    if pessoa == 1 and sexo in 'Mm':            #o nome do homem mas velho
+        maioridadehomem = idade
+        nomevelho = nome
+    if sexo in 'Mm' and idade > maioridadehomem:
+        maioridadehomem = idade
+        nomevelho = nome
+    if sexo in 'Ff' and idade < 20:     #Mostrando quantos mulheres tem com menos de 20 anos
+        totmulher20 += 1
 
-print('A média de idade do grupo é de {} anos'.format(idmedia))
+
+mediaidade = media / 4
+print('A média de idade do grupo é de {:.2f} anos'.format(mediaidade))
+print('O homem mas velho tem {} anos e se chama {}'.format(maioridadehomem, nomevelho))     #resultado final
+print('Ao todo são {} mulheres com menos de 20 anos'.format(totmulher20))
