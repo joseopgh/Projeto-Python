d6afdaa134c5907aaa87988c6c46cfb8fdfa43da
JoseDSJunior <145411583+joseopgh@users.noreply.github.com>
2025-08-12 19:59:34 -0300

    1006Media
    Calculando a media do aluno

---
+++ ExercBeecrowd/1006Media2.py
@@ -0,0 +1,8 @@
+notaA = float(input(''))
+notaB = float(input(''))
+notaC = float(input(''))
+provaA = 2
+provaB = 3
+provaC = 5
+media = (notaA * provaA + notaB * provaB + notaC * provaC) / (provaA + provaB + provaC)
+print('MEDIA = {:.1f}' .format(media))
