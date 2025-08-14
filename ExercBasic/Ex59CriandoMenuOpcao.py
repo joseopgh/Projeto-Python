b4b819b76bd08a1879fe87a1443a85ce9e0299fb
JoseDSJunior <145411583+joseopgh@users.noreply.github.com>
2025-08-13 21:51:13 -0300

    Ex059CriacaoMenuCompleto
    Melhoria do programa

--- ExercBasic/Ex59CriandoMenuOpcao.py
+++ ExercBasic/Ex59CriandoMenuOpcao.py
@@ -1,25 +1,32 @@
-
+from time import sleep
 valor1 = int(input('Primeiro valor: '))
 valor2 = int(input('segundo valor: '))
-print('''    [ 1 ] SOMA
+opcao = 0
+while opcao != 5:
+    print('''    [ 1 ] SOMA
     [ 2 ] MULTIPLICAR
     [ 3 ] MAIOR
     [ 4 ] NOVOS NÚMEROS
     [ 5 ] SAIR DO PROGRAMA''')
-opcao = int(input('>>>>> Qual é a sua opção? '))
+    opcao = int(input('>>>>> Qual é a sua opção? '))
 
     if opcao == 1:
         soma = valor1 + valor2
         print('A soma entre {} + {} é {}' .format(valor1, valor2, soma))
-    if opcao == 2:
+    elif opcao == 2:
         multiplica = valor1 * valor2
         print('A multiplicação entre {} x {} é {}' .format(valor1, valor2, multiplica))
-    if opcao == 3:
+    elif opcao == 3:
         maior = valor1 if valor1 > valor2 else valor2
         print('O maior numero entre {} e {} é {}' .format(valor1, valor2, maior))
-    if opcao == 4:
-        print('Escolha novos numeros')
-    if opcao == 5:
-        print('Programa encerrado!')
-    if opcao == 0 or opcao >= 6  :
+    elif opcao == 4:
+        print('Informe os números novamente: ')
+        valor1 = int(input('Primeiro valor: '))
+        valor2 = int(input('Segundo valor: '))
+    elif opcao == 5:
+        print('Finalizando...')
+    else:
         print('Opção invalida tente novamente!')
+    print('=-='*10)
+    sleep(1)
+print('Fim do programa!!')
