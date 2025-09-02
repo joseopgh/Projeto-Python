234bf8264c413e1b7cff0c1705e4bde9b44f8091
JoseDSJunior <145411583+joseopgh@users.noreply.github.com>
2025-09-02 19:17:58 -0300

    Ex62ProgressãoAritimetrica2.0
    Correção do codigo

--- ExercBasic/Ex062ProgressaoAritmetica2.0.py
+++ ExercBasic/Ex062ProgressaoAritmetica2.0.py
@@ -1,13 +1,16 @@
 print('GERADOR DE PA')
-print('-='*20)
-termo = int(input('Primeiro termo: '))
+print('-='*20)                                                     #cabeçario do codico
+termo = int(input('Primeiro termo: '))                             #introdução da variaveis
 razao = int(input('Razão da PA: '))
-cont = 1
-cont2 = 1
-while cont2 <= mtermo:
-    while cont <= 10:
+cont = 1                                                           #contador do while
+total = 0                                                          #contador do while termos
+mtermo = 10
+while mtermo != 0:                                                 #Laço para quantos mas voc queres
+    total = total + mtermo                                         #contador do laço
+    while cont <= total:                                           #primeiro laço
         print('{}'.format(termo), end=' -> ')
         termo = termo + razao
         cont = cont + 1
-    print('FIM')
-mtermo = int(input('Quantos termos você quer mostrar a mais? '))
+    print('PAUSA')
+    mtermo = int(input('Quantos termos você quer mostrar a mais? '))
+print('Progressão finalizada com {} termo mostrados.'.format(total))
